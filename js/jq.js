a129f162dde0b179cc38d11e3054e75e5bacbe10
dtf1168 <dtf5508@gmail.com>
2024-03-22 17:05:27 +0900

    푸터수정

---
+++ js/jq.js
@@ -0,0 +1,15 @@
+const pics = $('.pic');
+console.log(pics);
+const lightbox = $('#lightbox');
+const lightboxImage = $('#lightboxImage');
+pics.on('click', function (e) {
+	e.preventDefault();
+	const bigLocation = $(this).attr('data-src');
+	$('html').addClass('all_scroll_fixed');
+  lightbox.css('display','block');
+  lightboxImage.load(bigLocation);
+});
+lightbox.on('click',function(){
+ lightbox.css('display', 'none');
+ $('html').removeClass('all_scroll_fixed');
+})
